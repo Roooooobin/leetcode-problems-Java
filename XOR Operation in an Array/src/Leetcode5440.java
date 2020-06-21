8ef0d58d5f34a00dc17d445d62228f667254e2d0
Roooooobin <35558127+Roooooobin@users.noreply.github.com>
2020-06-21 21:27:21 +0800

    add leetcode-5440

---
+++ XOR Operation in an Array/src/Leetcode5440.java
@@ -0,0 +1,9 @@
+public class Leetcode5440 {
+    public int xorOperation(int n, int start) {
+        int ans = 0;
+        for (int i=0; i<n; ++i) {
+            ans ^= (start + 2 * i);
+        }
+        return ans;
+    }
+}
