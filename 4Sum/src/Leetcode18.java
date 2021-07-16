1c79f4b770ac14b9d5045fcc9e7d3b02ab18c910
Roooooobin <35558127+Roooooobin@users.noreply.github.com>
2021-07-17 07:11:46 +0800

    add leetcode-18

--- 4Sum/src/Leetcode18.java
+++ 4Sum/src/Leetcode18.java
@@ -8,26 +8,24 @@ public class Leetcode18 {
         int n = nums.length;
         List<List<Integer>> lists = new ArrayList<>();
         Arrays.sort(nums);
-        for(int i=0; i<n-3; ++i){
-            if(i > 0 && nums[i] == nums[i-1]) continue;
-            for(int j=i+1; j<n-2; ++j){
-                if(j > i+1 && nums[j] == nums[j-1]) continue;
+        for (int i = 0; i < n - 3; ++i) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            for (int j = i + 1; j < n - 2; ++j) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
                 int l = j + 1;
                 int r = n - 1;
-                while(l < r){
+                while (l < r) {
                     int sum = nums[l] + nums[r] + nums[i] + nums[j];
-                    if(sum == target){
+                    if (sum == target) {
                         lists.add(Arrays.asList(nums[i], nums[j], nums[l], nums[r]));
-                        // duplicates
-                        while(l < r && nums[l] == nums[l+1]) ++l;
-                        while(l < r && nums[r] == nums[r-1]) --r;
+                        // 去重
+                        while (l < r && nums[l] == nums[l + 1]) ++l;
+                        while (l < r && nums[r] == nums[r - 1]) --r;
                         ++l;
                         --r;
-                    }
-                    else if(sum < target){
+                    } else if (sum < target) {
                         ++l;
-                    }
-                    else{
+                    } else {
                         --r;
                     }
                 }
