4384c7f4f7135f5d19f166f59c27606b7ea4b58d
Roooooobin <35558127+Roooooobin@users.noreply.github.com>
2021-07-18 17:23:05 +0800

    add leetcode-316

--- Remove Duplicate Letters/src/Leetcode316.java
+++ Remove Duplicate Letters/src/Leetcode316.java
@@ -1,14 +1,32 @@
 public class Leetcode316 {
     public String removeDuplicateLetters(String s) {
-        int[] letters = new int[26];
+        // 记录字母出现次数
+        int[] letterCount = new int[26];
         for (int i = 0; i < s.length(); i++) {
-            letters[s.charAt(i) - 'a'] = 1;
+            letterCount[s.charAt(i) - 'a']++;
         }
+
+        // 只能出现一次，记录是否已经存在
+        boolean[] visited = new boolean[26];
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 26; i++) {
-            if (letters[i] == 1) {
-                sb.append((char)(i + 'a'));
+        for (int i = 0; i < s.length(); i++) {
+            char cur = s.charAt(i);
+            if (!visited[cur - 'a']) {
+                // 比当前大
+                while (sb.length() > 0 && sb.charAt(sb.length() - 1) > cur) {
+                    char top = sb.charAt(sb.length() - 1);
+                    // 还能删除
+                    if (letterCount[top - 'a'] > 0) {
+                        visited[top - 'a'] = false;
+                        sb.deleteCharAt(sb.length() - 1);
+                    } else {
+                        break;
+                    }
+                }
+                visited[cur - 'a'] = true;
+                sb.append(cur);
             }
+            letterCount[cur - 'a'] -= 1;
         }
         return sb.toString();
     }
