f617f1c0d2ef81778d24e1f829d7247f3f6516aa
Roooooobin <35558127+Roooooobin@users.noreply.github.com>
2021-07-19 18:41:23 +0800

    add leetcode-235

---
+++ Lowest Common Ancestor of a Binary Search Tree/src/Leetcode235.java
@@ -0,0 +1,22 @@
+public class Leetcode235 {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root.equals(p) || root.equals(q)) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        // separate locate in left and right
+        if (left != null && right != null) return root;
+        // one is another one's ancestor
+        return left == null ? right : left;
+    }
+}
