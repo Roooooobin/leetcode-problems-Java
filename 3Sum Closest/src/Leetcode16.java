208100b7f1c210aa399365c897d7164517fef14b
Roooooobin <35558127+Roooooobin@users.noreply.github.com>
2021-07-28 07:44:21 +0800

    add leetcode-16

--- 3Sum Closest/src/Leetcode16.java
+++ 3Sum Closest/src/Leetcode16.java
@@ -5,20 +5,20 @@ public class Leetcode16 {
         Arrays.sort(nums);
         int n = nums.length;
         int closest = nums[0] + nums[1] + nums[2];
-        for(int i=0; i<n-2; ++i){
+        for (int i = 0; i < n - 2; ++i) {
             int j = i + 1, k = n - 1;
-            while(j < k){
+            while (j < k) {
                 int sum = nums[i] + nums[j] + nums[k];
-                if(Math.abs(sum - target) < Math.abs(closest - target)){
+                if (Math.abs(sum - target) < Math.abs(closest - target)) {
                     closest = sum;
                 }
-                if(sum > target){
-                    --k;
-                }
-                else if(sum < target){
+                if (sum < target) {
                     ++j;
+                } else if (sum > target) {
+                    --k;
+                } else {
+                    return target;
                 }
-                else return target;
             }
         }
         return closest;
