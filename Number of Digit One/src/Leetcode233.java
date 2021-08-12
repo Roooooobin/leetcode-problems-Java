5dfa3fed21b9e2d00e7d0b57f273c6a44150918c
Roooooobin <35558127+Roooooobin@users.noreply.github.com>
2021-08-13 07:14:32 +0800

    add leetcode-233

--- Number of Digit One/src/Leetcode233.java
+++ Number of Digit One/src/Leetcode233.java
@@ -1,7 +1,7 @@
 public class Leetcode233 {
     public int countDigitOne(int n) {
         int ans = 0;
-        for (long i=1; i<=n; i*=10) {
+        for (long i = 1; i <= n; i *= 10) {
             long div = i * 10;
             ans += (n / div) * i + Math.min(Math.max(n % div - i + 1, 0), i);
         }
