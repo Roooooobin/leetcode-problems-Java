b40fa53b1d6231d76d472cdb0c46b31b0011f6d2
Roooooobin <lz2294@nyu.edu>
2021-09-11 10:39:42 +0800

    add leetcode-1986(state-compression dp)

--- Minimum Number of Work Sessions to Finish the Tasks/src/Leetcode1986.java
+++ Minimum Number of Work Sessions to Finish the Tasks/src/Leetcode1986.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Leetcode1986 {
 
     int[] tasks;
@@ -31,6 +33,44 @@ public class Leetcode1986 {
                 dfs(a, idx + 1);
                 a[i] -= tasks[idx];
             }
+        }
+    }
+
+    // 状压dp
+    // 二进制位值为1表示该工人工作
+    class Solution {
+
+        public int minSessions(int[] tasks, int sessionTime) {
+
+            int n = tasks.length;
+            int m = 1 << n;
+            int[] dp = new int[m];
+            Arrays.fill(dp, n);
+            // 预处理，按照位计算能否在一段session中完成
+            for (int i = 1; i < m; i++) {
+                int x = i, idx = 0;
+                int time = 0;
+                while (x > 0) {
+                    if ((x & 1) == 1) {
+                        time += tasks[idx];
+                    }
+                    x >>= 1;
+                    idx++;
+                }
+                if (time <= sessionTime) {
+                    dp[i] = 1;
+                }
+            }
+
+            for (int i = 1; i < m; i++) {
+                if (dp[i] == 1) {
+                    continue;
+                }
+                for (int j = i; j > 0; j = (j - 1) & i) {
+                    dp[i] = Math.min(dp[i], dp[j] + dp[i ^ j]);
+                }
+            }
+            return dp[m - 1];
         }
     }
 }
