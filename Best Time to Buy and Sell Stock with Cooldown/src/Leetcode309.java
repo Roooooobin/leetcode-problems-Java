4e04748e8632a9ac49f5c18787d32c153c9b63cd
Roooooobin <lz2294@nyu.edu>
2021-10-15 14:39:13 +0800

    add leetcode-309

--- Best Time to Buy and Sell Stock with Cooldown/src/Leetcode309.java
+++ Best Time to Buy and Sell Stock with Cooldown/src/Leetcode309.java
@@ -11,4 +11,22 @@ public class Leetcode309 {
         }
         return dp0;
     }
+
+    public int maxProfit(int[] prices) {
+
+        int n = prices.length;
+        /*
+        dp[i][0] with stock
+        dp[i][1] without stock and in cooldown
+        dp[i][2] without stock and not in cooldown
+         */
+        int[][] dp = new int[n][3];
+        dp[0][0] = -prices[0];
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][2] - prices[i]);
+            dp[i][1] = dp[i - 1][0] + prices[i];
+            dp[i][2] = Math.max(dp[i - 1][1], dp[i - 1][2]);
+        }
+        return Math.max(dp[n - 1][1], dp[n - 1][2]);
+    }
 }
